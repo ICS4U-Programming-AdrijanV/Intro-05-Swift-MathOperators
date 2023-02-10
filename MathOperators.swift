3f983ba4536dfcf250be803ff03dd6b7e822bc66
Adrijan Vranjkovic <adrijan.vranjkovic@stu.ocsb.ca>
2023-02-10 02:35:58 +0000

    Mathoperators

---
+++ MathOperators.swift
@@ -0,0 +1,17 @@
+// Importing math operations
+import Foundation
+
+// making float and double
+var dividingFloat :Float = 11 / 6
+let dividingDouble :Double = 11 / 6
+
+// Print math operators.
+print("8 + 3 = \(8 + 3)")
+print("6 - 5 = \(6 - 5)")
+print("5 * 4 = \(5 * 4)")
+print("8 / 4 = \(8 / 4)")
+print("11 / 6 = \(dividingFloat)")
+print("11 / 6 = \(11 / 6)")
+print("7^2 = \(pow(7, 2))")
+print("4^3 = \(pow(4, 3))")
+print("√36 = \(sqrt(36))")
